9d758b118c84bb9e72a32c751d5b12d88e363767
MSvyda <Mykhailo.Svyda@cs.khpi.edu.ua>
2025-05-08 15:35:15 +0300

    додав фільтри та переробив структуру cars

--- server/internal/handler/car_handler.go
+++ server/internal/handler/car_handler.go
@@ -2,7 +2,9 @@ package handler
 
 import (
 	"net/http"
+	"strconv"
 
+	"github.com/exideys/car_rental_service/internal/models"
 	"github.com/exideys/car_rental_service/internal/service"
 	"github.com/gin-gonic/gin"
 )
@@ -16,10 +18,24 @@ func NewCarHandler(svc service.CarService) *CarHandler {
 }
 
 func (h *CarHandler) GetAvailableCars(c *gin.Context) {
-	cars, err := h.svc.ListAvailableCars()
+	filters := models.CarFilters{
+		Brand:         c.Query("brand"),
+		TransportType: c.Query("transport_type"),
+		BodyType:      c.Query("body_type"),
+		Transmission:  c.Query("transmission"),
+		MinPrice:      parseFloat(c.Query("min_price")),
+		MaxPrice:      parseFloat(c.Query("max_price")),
+	}
+
+	cars, err := h.svc.GetFilteredCars(filters)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 	c.JSON(http.StatusOK, cars)
+}
+
+func parseFloat(value string) float64 {
+	f, _ := strconv.ParseFloat(value, 64)
+	return f
 }
