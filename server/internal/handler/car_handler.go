bd7380acd622bdafd37250bd45f0e05e08d14222
Anton <oaoaosoopoa@gmail.com>
2025-05-14 21:53:42 +0200

    Update car_handler.go

--- server/internal/handler/car_handler.go
+++ server/internal/handler/car_handler.go
@@ -2,9 +2,7 @@ package handler
 
 import (
 	"net/http"
-	"strconv"
 
-	"github.com/exideys/car_rental_service/internal/models"
 	"github.com/exideys/car_rental_service/internal/service"
 	"github.com/gin-gonic/gin"
 )
@@ -18,24 +16,10 @@ func NewCarHandler(svc service.CarService) *CarHandler {
 }
 
 func (h *CarHandler) GetAvailableCars(c *gin.Context) {
-	filters := models.CarFilters{
-		Brand:         c.Query("brand"),
-		TransportType: c.Query("transport_type"),
-		BodyType:      c.Query("body_type"),
-		Transmission:  c.Query("transmission"),
-		MinPrice:      parseFloat(c.Query("min_price")),
-		MaxPrice:      parseFloat(c.Query("max_price")),
-	}
-
-	cars, err := h.svc.GetFilteredCars(filters)
+	cars, err := h.svc.ListAvailableCars()
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 	c.JSON(http.StatusOK, cars)
-}
-
-func parseFloat(value string) float64 {
-	f, _ := strconv.ParseFloat(value, 64)
-	return f
 }
