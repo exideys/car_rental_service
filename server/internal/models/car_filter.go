1f62200a8f507b0f50a0941d0754dc1d7e18ab2c
AntonKhPI2 <oaoaosoopoa@gmail.com>
2025-05-17 21:34:22 +0200

    Fixed filter

--- server/internal/models/car_filter.go
+++ server/internal/models/car_filter.go
@@ -1,9 +1,9 @@
 package models
 
-type Car_filter struct {
-	Min_price      uint32   `form:"price-from"`
-	Max_price      uint32   `form:"price-to"`
-	Car_brand      string   `form:"car-brand"`
-	Year_of_issues uint16   `form:"Year_of_issues"`
-	Car_class      []string `form:"car-class" binding:"omitempty,dive,oneof=Premium Comfort Econom"`
+type CarFilter struct {
+	MinPrice     uint32   `form:"price-from"`
+	MaxPrice     uint32   `form:"price-to"`
+	CarBrand     string   `form:"car-brand"`
+	YearOfIssues uint16   `form:"year_of_issues"`
+	CarClass     []string `form:"car-class" binding:"omitempty,dive,oneof=Premium Comfort Econom"`
 }
