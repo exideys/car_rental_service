e1ebec2986fd64ce3741b086be7f5898c5613cf8
AntonKhPI2 <oaoaosoopoa@gmail.com>
2025-05-23 18:11:39 +0200

    fixed about.html and car filter

--- server/internal/models/car_filter.go
+++ server/internal/models/car_filter.go
@@ -3,7 +3,7 @@ package models
 type CarFilter struct {
 	MinPrice     uint32   `form:"price-from"`
 	MaxPrice     uint32   `form:"price-to"`
-	CarBrand     string   `form:"car-brand"`
-	YearOfIssues uint16   `form:"year_of_issues"`
+	CarBrand     string   `form:"car_brand"`
+	YearOfIssues uint16   `form:"year_of_issue"`
 	CarClass     []string `form:"car-class" binding:"omitempty,dive,oneof=Premium Comfort Econom"`
 }
